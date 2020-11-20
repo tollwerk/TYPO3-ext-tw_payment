9e98c264261db17b87ebba130319b3c4e50270d5
Klaus <klaus@tollwerk.de>
2020-11-20 11:28:38 +0100

    Update stripe/stripe-php to v7

--- Resources/Private/Vendor/stripe/stripe-php/lib/ApiResponse.php
+++ Resources/Private/Vendor/stripe/stripe-php/lib/ApiResponse.php
@@ -2,20 +2,38 @@
 
 namespace Stripe;
 
+use Stripe\Util\CaseInsensitiveArray;
+
+/**
+ * Class ApiResponse.
+ */
 class ApiResponse
 {
+    /**
+     * @var null|array|CaseInsensitiveArray
+     */
     public $headers;
+
+    /**
+     * @var string
+     */
     public $body;
+
+    /**
+     * @var null|array
+     */
     public $json;
+
+    /**
+     * @var int
+     */
     public $code;
 
     /**
      * @param string $body
-     * @param integer $body
-     * @param array|null $headers
-     * @param array|null $json
-     *
-     * @return obj An APIResponse
+     * @param int $code
+     * @param null|array|CaseInsensitiveArray $headers
+     * @param null|array $json
      */
     public function __construct($body, $code, $headers, $json)
     {
